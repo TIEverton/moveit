75e9794e3eafda30b73f59f1cf917725a47cbd66
evertonpinheiroti <evertonpinheiroti@gmail.com>
2021-03-08 18:21:55 -0300

    :tada: Initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/tieverton.png" alt="Everton Pinheiro" />
+      <div>
+        <strong> Everton Pinheiro </strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
