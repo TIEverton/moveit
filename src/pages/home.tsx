18a7b6bf29ecb5fd3e3679b9e53093c846064d59
Everton Pinheiro <evertonpinheiroti@hotmail.com>
2021-03-13 20:19:27 -0300

    :sparkles: Adding page switching

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
@@ -6,32 +6,55 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CountdownContext';
-import Sidebar from '../components/Sidebar';
 
 import styles from '../styles/pages/Home.module.css'
+import { FiAward, FiHome, FiLogOut } from 'react-icons/fi';
+import Leaderboard from '../components/Leaderboard';
 
 export default function Home() {
+  const [isClicked, setIsClicked] = useState(true);
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Home | move.it</title>
-      </Head>
-      <Sidebar />
-      <div className={styles.content}>
-        <ExperienceBar />
 
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
-            </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
+      <Head>
+        {isClicked ? <title>Home | move.it</title> : <title>Leaderboard | move.it</title>}
+      </Head>
+
+      <div className={styles.containerSidebar}>
+
+        <img src="/logo.svg" alt="Move.it" />
+        <div>
+          <ul>
+            <li className={isClicked ? styles.clicked : ''} onClick={() => setIsClicked(true)}><FiHome size={32} title="Home" /></li>
+            <li className={isClicked ? '' : styles.clicked} onClick={() => setIsClicked(false)}><FiAward size={32} title="Leaderboard" /></li>
+          </ul>
+        </div>
+        <FiLogOut size={24} title="Logout" />
+      </div>
+
+      <div className={styles.content}>
+        {isClicked ? (
+          <>
+            <ExperienceBar />
+
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile />
+                  <CompletedChallenges />
+                  <Countdown />
+                </div>
+                <div>
+                  <ChallengeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </>
+        ) : (
+          <Leaderboard />
+        )}
+
       </div>
     </div>
   )
