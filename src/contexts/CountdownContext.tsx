fe4bf8993a5f619cf8e9c3deddfb943142a0e0cb
Everton Pinheiro <evertonpinheiroti@hotmail.com>
2021-03-16 11:55:34 -0300

    :sparkles: Update countdown time

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export const CountdownContext = createContext({} as CountdownContextData)
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(0.05 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -48,7 +48,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(coundownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(0.05 * 60);
   }
 
 
