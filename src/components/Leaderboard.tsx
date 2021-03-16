996b9dd64d6a21d49b458e9c566765200640211a
Everton Pinheiro <evertonpinheiroti@hotmail.com>
2021-03-16 11:58:05 -0300

    :sparkles: Integration Leaderboard screen API integration with Fake API

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,90 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
 
 import styles from '../styles/components/Leaderboard.module.css';
 
+interface DataProps {
+  id: string,
+  name: string,
+  avatar_url: string,
+  level: string,
+  currentExperience: string,
+  challengesCompleted: string,
+}
+
 export default function Leaderboard() {
+  const [data, setData] = useState<DataProps[]>([]);
+
+  useEffect(() => {
+    const res = api.get('/users').then(response => {
+      const order = response.data.sort(function (a, b) {
+        return a.level > b.level || a.currentExperience > b.currentExperience ? -1 : '';
+      })
+      setData(order)
+    })
+
+
+  }, [])
+
   return (
     <div className={styles.containerLeaderboard}>
       <div className={styles.contentLeader}>
         <h1>Leaderboard</h1>
-        <table className={styles.tableLeader} cellspacing="0">
-          <tr>
-            <th>POSIÇÃO</th>
-            <th>USUÁRIO</th>
-            <th>DESAFIOS</th>
-            <th>EXPERIÊNCIAS</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>
-              <div className={styles.user}>
-                <img src="https://avatars.githubusercontent.com/u/44790240?v=4" alt="" />
-                <div>
-                  <strong>Everton Pinheiro</strong>
-                  <div className={styles.level}>
-                    <img src="icons/level.svg" alt="" />
-                    <p>Level 36</p>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td><strong>123</strong> completados</td>
-            <td><strong>12121</strong> xp</td>
-          </tr>
-          <div className={styles.separator} />
-          <tr>
-            <td>1</td>
-            <td>
-              <div className={styles.user}>
-                <img src="https://avatars.githubusercontent.com/u/44790240?v=4" alt="" />
-                <div>
-                  <strong>Everton Pinheiro</strong>
-                  <div className={styles.level}>
-                    <img src="icons/level.svg" alt="" />
-                    <p>Level 36</p>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td><strong>123</strong> completados</td>
-            <td><strong>12121</strong> xp</td>
-          </tr>
-          <div className={styles.separator} />
-          <tr>
-            <td>1</td>
-            <td>
-              <div className={styles.user}>
-                <img src="https://avatars.githubusercontent.com/u/44790240?v=4" alt="" />
-                <div>
-                  <strong>Everton Pinheiro</strong>
-                  <div className={styles.level}>
-                    <img src="icons/level.svg" alt="" />
-                    <p>Level 36</p>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td><strong>123</strong> completados</td>
-            <td><strong>12121</strong> xp</td>
-          </tr>
-          <div className={styles.separator} />
-          <tr>
-            <td>1</td>
-            <td>
-              <div className={styles.user}>
-                <img src="https://avatars.githubusercontent.com/u/44790240?v=4" alt="" />
-                <div>
-                  <strong>Everton Pinheiro</strong>
-                  <div className={styles.level}>
-                    <img src="icons/level.svg" alt="" />
-                    <p>Level 36</p>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td><strong>123</strong> completados</td>
-            <td><strong>12121</strong> xp</td>
-          </tr>
+        <table className={styles.tableLeader}>
+          <thead>
+            <tr>
+              <th>POSIÇÃO</th>
+              <th>USUÁRIO</th>
+              <th>DESAFIOS</th>
+              <th>EXPERIÊNCIAS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <>
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className={styles.user}>
+                      <img src={user.avatar_url} alt={user.name} />
+                      <div>
+                        <strong>{user.name}</strong>
+                        <div className={styles.level}>
+                          <img src="icons/level.svg" alt="" />
+                          <p>Level {user.level}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td><strong>{user.challengesCompleted}</strong> completados</td>
+                  <td><strong>{user.currentExperience}</strong> xp</td>
+                </tr>
+                <div className={styles.separator} />
+              </>
+            ))}
+          </tbody>
         </table>
 
 
